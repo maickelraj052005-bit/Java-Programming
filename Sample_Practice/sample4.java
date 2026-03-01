afe50aeeb947f77dda7ab25cd4dcd29fee61a4e8
maickelraj S <maickelraj052005@gmail.com>
2026-03-01 16:45:52 +0530

    Add files via upload

---
+++ Sample_Practice/sample4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class sample4 {
+    public static void main(String[] args) {
+    
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        sc.nextLine();
+        String str=sc.nextLine();
+        
+        System.out.println((float)a);
+        System.out.println(str);
+
+    }
+}
