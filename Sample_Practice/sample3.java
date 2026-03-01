cdc87c42004c682ca5968aad1c02e50d32b06004
maickelraj S <maickelraj052005@gmail.com>
2026-03-01 16:44:37 +0530

    Add files via upload

---
+++ Sample_Practice/sample3.java
@@ -0,0 +1,9 @@
+public class sample3 {
+    public static void main(String[] args) {
+        String name = "Maickel";
+        String name1 = "maickel";
+
+        System.out.println(name==name1);
+        System.out.println();
+    }
+}
