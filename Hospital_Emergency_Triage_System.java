00ea1c93047c8da66f74b20505e11d78c84719f6
maickelraj S <maickelraj052005@gmail.com>
2026-03-09 21:10:38 +0530

    Add files via upload

---
+++ Hospital_Emergency_Triage_System.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Hospital_Emergency_Triage_System {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int heartRate = sc.nextInt();
+        sc.nextLine();
+        String bloodPressure =sc.nextLine();
+        double temperature =sc.nextDouble();
+        sc.nextLine();
+        String symptomSeverity = sc.nextLine();
+
+        String Triage_Priority = "";
+        double Estimated_Wait_Time =0;
+        String Recommended_Action = "";
+
+
+
+        //  To be continue ;
+
+
+
+    }
+}
