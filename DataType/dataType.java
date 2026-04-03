b2dc52170583dc7c86f62adedcb1f138e7725cfb
maickelraj S <maickelraj052005@gmail.com>
2026-04-03 21:24:23 +0530

    Add files via upload

---
+++ DataType/dataType.java
@@ -0,0 +1,25 @@
+public class dataType {
+
+    public static void main(String[] args) {
+        // Primitive DataType...
+        int number = 10;
+        float deci = 23.58f;
+        double maxDeci = 58.0950;
+        boolean isboolean = true;
+        char ch = 'A';
+        short hl = 895;
+        
+        // Non-primitive data Type.....
+        String name = "Maickelraj";
+
+
+
+        System.out.println("Int"+ number);
+        System.out.println(deci);
+        System.out.println(maxDeci);
+        System.out.println(isboolean);
+        System.out.println(ch);
+        System.out.println(hl);
+        System.out.println(name);
+    }
+}
