8e088b042c68ab7be80cc472f106211e1a7c2245
maickelraj S <maickelraj052005@gmail.com>
2026-04-26 20:16:40 +0530

    Refactor loan approval logic in BankLoanApprovalSystem

--- BankLoanApprovalSystem.java
+++ BankLoanApprovalSystem.java
@@ -1,6 +1,6 @@
 import java.util.Scanner; 
  
-public class BankLoanApprovalSystem {
+public class BankLoanApprovalSystem { 
     public static void main(String[] args) {  
 
         Scanner sc = new Scanner(System.in);
