d16cd008208f13bfd5c9318b6928410593ca5964
maickelraj S <maickelraj052005@gmail.com>
2026-04-26 20:15:49 +0530

    Update BankLoanApprovalSystem.java

--- BankLoanApprovalSystem.java
+++ BankLoanApprovalSystem.java
@@ -1,7 +1,7 @@
 import java.util.Scanner; 
  
 public class BankLoanApprovalSystem {
-    public static void main(String[] args) { 
+    public static void main(String[] args) {  
 
         Scanner sc = new Scanner(System.in);
    
